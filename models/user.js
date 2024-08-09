71a10a540c04b5b991d517bd98fc0396f7eeb61f
BeauX <fire-fly_7@hotmail.com>
2024-08-09 11:31:19 -0700

    nav

--- models/user.js
+++ models/user.js
@@ -9,8 +9,21 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  pantry: [{ type: mongoose.Schema.types.ObjectId, ref: 'Food'}]
 });
 
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
+
+
+const foodSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  //category: { type: String, required: true },
+  //vegan: Boolean,
+  // glutenFree: Boolean
+});
+
+const Food = model('Food', foodSchema);
+
+module.exports = Food;
